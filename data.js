b8038ae836e55eb26e6c99826e8753860a3e4785
Javmir Gamma <javmir.hernandez@students.gammatech.schoolgit config --global user.email javmir.hernandez@students.gammatech.school>
2024-06-28 00:40:38 +0200

    cards created

--- data.js
+++ data.js
@@ -1,104 +1,102 @@
-
-
-const items = [
+export  const items = [
     {
-        id: cec0b86f43364b97bb30aec54360fc2a,
-        product_Name : "Hamburguer",
+        id: 'e9c6bcd48a5b460c7c0b01de4b3ed269',
+        product_Name : "Hamburger 🍔",
         isToyAvailable: true,
         description: "lorem ipsum",
         ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        price: 15,
         score:6,
         img:"https://cocina-casera.com/wp-content/uploads/2014/10/hamburguesa-perfecta-.jpg"
     },
     {
-        id: e9c6bcd48a5b460c8c0b01de4b3ed269,
-        product_Name : "Pepito",
+        id: 'e9c6bcd48a5b460c8c0b01de4b3ed269',
+        product_Name : "Kebab 🥙",
         isToyAvailable: false,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "El kebab es un plato originario del Medio Oriente, que consiste en carne asada o a la parrilla, comúnmente de cordero, servida en un pan pita con vegetales frescos y salsa de yogur.",
+        ingredients: ["carne de cordero", "lechuga", "tomate", "salsa de yogur", "pan pita"],
+        price: 5,
         score:6,
-        img: "https://cocina-casera.com/wp-content/uploads/2014/10/hamburguesa-perfecta-.jpg"
+        img: "https://www.gourmetkebab.es/wp-content/uploads/2021/04/kebab-que-es.jpg"
     },
     {
-        id: efd467afe4c04c0e96ea0d467398a176,
-        product_Name : "Tacos",
+        id: 'efd467afe4c04c0e96ea0d467398a176',
+        product_Name : "Tacos 🌮",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "Los tacos son una comida tradicional mexicana, hecha con tortillas de maíz o harina que se rellenan con una variedad de ingredientes como carne, cebolla, cilantro y salsa",
+        ingredients: ["tortilla de maíz", "carne de res", "cebolla", "cilantro", "salsa"],
+        price: 1,
         score:6,
-        img: "https://stock.adobe.com/es/images/taco-on-white-background/233513752"
+        img: "https://7televalencia.com/wp-content/uploads/2022/10/taquito.png"
     },
     {
-        id: eda0b6f16975443f8d977491c9526383,
-        product_Name : "Hot dog",
+        id: 'eda0b6f16975443f8d977491c9526383',
+        product_Name : "Hot dog 🌭",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "El hot dog es un tipo de bocadillo popular en Estados Unidos, compuesto por una salchicha cocida o asada servida en un pan alargado, y suele acompañarse con mostaza, ketchup y cebolla.",
+        ingredients: ["pan de hot dog", "salchicha", "mostaza", "ketchup", "cebolla"],
+        price: 3,
         score:6,
-        img: "https://img.freepik.com/free-vector/fast-food-sticker-design-with-hot-dog-isolated_1308-67129.jpg?w=1480&t=st=1719423922~exp=1719424522~hmac=df558659db13da26b331e4eba31bae755b37d2a2a5f80da73ae2ebc5b85c7330"
+        img: "https://www.foodandwine.com/thmb/0fO2iubr24kzX7Ky33S5-p0R7yo=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/best-hot-dogs-in-every-state-FT-BLOG1020-6e025500cefb480ba986b163792ec790.jpg"
     },
     {
-        id: a85c6e09c7164e1aaf7ab31a2d58bb07,
-        product_Name : "Pizza",
+        id: 'a85c6e09c7164e1aaf7ab31a2d58bb07',
+        product_Name : "Pizza 🍕",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "La pizza es un plato italiano que consiste en una base de masa cubierta con salsa de tomate, queso y diversos ingredientes adicionales como pepperoni, aceitunas y vegetales, horneada hasta quedar crujiente.",
+        ingredients: ["masa de pizza", "salsa de tomate", "queso mozzarella", "pepperoni", "aceitunas"],
+        price: 8,
         score:6,
         img: "https://cdn2.cocinadelirante.com/800x600/filters:format(webp):quality(75)/sites/default/files/images/2023/08/receta-de-pizza-sin-horno.jpg"
     },
     {
-        id: rec0fe8eaf3f4c11a65bd5fb44e914ac,
-        product_Name : "Cachapa",
+        id: 'rec0fe8eaf3f4c11a65bd5fb44e914ac',
+        product_Name : "Cachapa 🥞",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "La cachapa es una especie de panqueque venezolano hecho de maíz tierno molido, típicamente servido con queso de mano y otros rellenos como jamón o carne mechada.",
+        ingredients: ["masa de maíz", "queso de mano", "mantequilla", "jamón", "carne mechada"],
+        price: 14,
         score:6,
         img: "https://media.istockphoto.com/id/1008389336/es/foto/nost%C3%A1lgicos-con-queso-plato-t%C3%ADpico-venezolano-hecho-con-ma%C3%ADz-queso-y-mantequilla.jpg?s=1024x1024&w=is&k=20&c=5ElSzULgfk3DccmQMT1sJX6QnRkw9VQFH0-R9T3BJZ8="
     },
     {
-        id: b351afdaa0a047248e5a8c93f042b989,
-        product_Name : "Arepa",
+        id: 'b351afdaa0a047248e5a8c93f042b989',
+        product_Name : "Arepa 🫓",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "La arepa es un alimento tradicional de Venezuela y Colombia, hecho de masa de harina de maíz que se cocina a la plancha, al horno o frita, y se rellena con diversos ingredientes como queso, pollo desmechado o carne.",
+        ingredients: ["harina de maíz", "agua", "sal", "queso", "pollo desmechado"],
+        price: 9,
         score:6,
-        img: "https://static.kfc.es/images/items/lg/41290.jpg?v=3mEWJ3"
+        img: "https://i.blogs.es/8ad4da/arepas/1366_2000.jpg"
     },
     {
-        id: aqwdfd2eddd34fc9b9c930c775ff6407,
-        product_Name : "Empanada",
+        id: 'aqwdfd2eddd34fc9b9c930c775ff6407',
+        product_Name : "Empanada 🥟",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne"],
-        price: 20,
+        description: "La empanada es una preparación culinaria que consiste en una masa rellena con una mezcla de carne, cebolla, pimiento y a veces huevo duro, que se cierra en forma de media luna y se fríe o hornea.",
+        ingredients: ["masa de harina de maíz", "carne molida","jamon y queso","carne mechada"],
+        price:6,
         score:6,
         img: "https://peopleenespanol.com/thmb/cNXa1V3R1Akf0hECIcQNr3fFwPI=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1158987157-2000-25ff8f49b6af4027ac4a62eed28effd7.jpg"
     },
     {
-        id: 7add55e100f5416bb519a730389d8d11,
-        product_Name : "Burrito",
+        id: 'vadd55e100f5416bb519a730389d8d11',
+        product_Name : "Burrito 🌯",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["lechuga", "tomate", "pan", "mayo", "ketchup", "Queso","bacon", "carne","frijol"],
-        price: 20,
+        description: "El burrito es un platillo mexicano-americano compuesto por una tortilla de harina enrollada alrededor de un relleno que puede incluir frijoles, arroz, carne, queso y otros ingredientes",
+        ingredients: ["tortilla de harina", "frijoles", "arroz", "carne de res", "queso"],
+        price: 11,
         score:6,
         img: "https://deltaco.com/files/menu/item/DEL-116_Promo4_Product_Hero_Image_Epic_Fresh_860x480.png?v=4.039"
     },
     {
-        id: cce84ccd504447bdb3c121e879e4b5dd,
-        product_Name : "KFC",
+        id: 'cce84ccd504447bdb3c121e879e4b5dd',
+        product_Name : "KFC 🍗",
         isToyAvailable: true,
-        description: "lorem ipsum",
-        ingredients: ["alitas","pechuga"],
-        price: 20,
+        description: "KFC es una cadena de comida rápida conocida por su pollo frito, que se caracteriza por estar rebozado con una mezcla secreta de especias y hierbas, y luego frito hasta quedar crujiente.",
+        ingredients: ["pollo", "harina", "especias", "aceite para freír", "sal"],
+        price: 13,
         score:6,
         img: "https://static.kfc.es/images/items/lg/41290.jpg?v=3mEWJ3"
     }
-]
+];
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+import { items } from "./data.js";
 const body = document.querySelector("body");
 
 const div = document.querySelector(".div");
+
+const carrito = [];
+
 
 const buttonShow = document.querySelector("#button")
 
@@ -16,11 +20,12 @@ if(divWithAbsolute.style.display === "none"){
 
 
 const divContainer = document.createElement('div');
+divContainer.classList.add('productCard');
 const divImg = document.createElement('div');
 const img = document.createElement('img')
 const divDescription = document.createElement('div'); 
-const h2 = document.createElement('h2');
-const h3 = document.createElement('h3');
+const pProductName = document.createElement('p');
+const pPriceProduct = document.createElement('p');
 const buttonCard = document.createElement('button');
 
 body.style.flexDirection = 'column'
@@ -28,21 +33,59 @@ body.appendChild(divContainer);
 divContainer.appendChild(divImg)
 divImg.appendChild(img)
 divContainer.appendChild(divDescription);
-divDescription.appendChild(h2);
-divDescription.appendChild(h3);
+divDescription.appendChild(pProductName);
+divDescription.appendChild(pPriceProduct);
 divContainer.appendChild(buttonCard)
 
 divContainer.style.width = "150px";
 divContainer.style.height = "300px"; 
 divContainer.style.backgroundColor = "white";
 divContainer.style.borderRadius = "16px";
+divContainer.style.marginTop = "15px";
+divContainer.style.display = "flex";
+divContainer.style.flexDirection = 'column';
+divContainer.style.alignItems= 'center';
+divContainer.style.padding = '10px'; 
+// divContainer.style.justifyContent = 'space-between';
+divContainer.style.boxSizing = 'border-box';
+divContainer.style.gap= '12px';
 
-h2.innerHTML = "Tacos";
-h3.innerHTML = "15€"
 
+
+divImg.style.borderRadius ='8px'
+divImg.style.border ='1px solid red'
+divImg.style.width = '90%'
+divImg.style.height = '90px'
+divImg.style.overflow ='hidden'
+divImg.style.boxSizing = 'border-box';
 img.src = "https://7televalencia.com/wp-content/uploads/2022/10/taquito.png";
-img.style.width = "110px"
-img.style.height = "90px"
+img.style.width = "100%";
+img.style.height = "90px";
+img.style.objectFit ='cover';
+
+divDescription.style.display= 'flex';
+divDescription.style.flexDirection= 'column';
+divDescription.style.alignItems= 'center';
+divDescription.style.gap= '12px';
+divDescription.style.flexGrow = '1'
+divDescription.style.flexShrink = '1'
+
+pProductName.innerHTML = "Tacos";
+pProductName.style.fontSize = '16px';
+pProductName.style.padding = '0';
+pProductName.style.margin = '0';
+pPriceProduct.style.padding = '0';
+pPriceProduct.style.margin = '0';
+pPriceProduct.innerHTML = "15€"
+pPriceProduct.style.fontSize = '12px';
+
+
+
+buttonCard.style.width = '40px'
+buttonCard.style.fontSize = '12px'
+buttonCard.style.height = '20px'
+buttonCard.innerText = 'info'
+
 
 
 //Dentro de html
@@ -50,3 +93,121 @@ img.style.height = "90px"
 //APPEND CHILD CARTA AUTOMATICA
 
 //CONSOLOG LA INFORMACION DE CARTA PRINCIPAL
+
+
+
+
+
+const containerBodyFood= document.createElement('div');
+containerBodyFood.classList.add('containerMenuFood')
+containerBodyFood.style.display = 'flex';
+containerBodyFood.style.flexWrap = 'wrap';
+containerBodyFood.style.justifyContent = 'center';
+containerBodyFood.style.gap = '15px'
+body.appendChild(containerBodyFood);
+
+
+for (const item of items) {
+const divContainer = document.createElement('div');
+divContainer.classList.add('productCard');
+const divImg = document.createElement('div');
+
+const img = document.createElement('img')
+img.setAttribute('src',item.img);
+
+const divDescription = document.createElement('div');
+divDescription.classList.add('productInfo');
+const pProductName = document.createElement('p');
+pProductName.innerHTML = item.product_Name;
+const pPriceProduct = document.createElement('p');
+pPriceProduct.innerHTML = item.price + ' €';
+
+const buttonCard = document.createElement('button');
+buttonCard.innerHTML ='info';   
+
+body.appendChild(divContainer);
+divContainer.appendChild(divImg)
+divImg.appendChild(img)
+divContainer.appendChild(divDescription);
+divDescription.appendChild(pProductName);
+divDescription.appendChild(pPriceProduct);
+divContainer.appendChild(buttonCard)
+
+//Styles
+divContainer.style.width = "150px";
+divContainer.style.height = "300px"; 
+divContainer.style.backgroundColor = "white";
+divContainer.style.borderRadius = "16px";
+divContainer.style.marginTop = "15px";
+divContainer.style.display = "flex";
+divContainer.style.flexDirection = 'column';
+divContainer.style.alignItems= 'center';
+divContainer.style.padding = '10px'; 
+// divContainer.style.justifyContent = 'space-between';
+divContainer.style.boxSizing = 'border-box';
+divContainer.style.gap= '12px';
+divContainer.style.fontFamily = 'Poppins'
+
+
+
+divImg.style.borderRadius ='10px 10px 0px 0px'
+// divImg.style.border ='1px solid red'
+divImg.style.width = '90%'
+divImg.style.height = '90px'
+divImg.style.overflow ='hidden'
+divImg.style.boxSizing = 'border-box';
+// img.src = "https://7televalencia.com/wp-content/uploads/2022/10/taquito.png";
+img.style.width = "100%";
+img.style.height = "90px";
+img.style.objectFit ='cover';
+
+divDescription.style.display= 'flex';
+divDescription.style.flexDirection= 'column';
+divDescription.style.alignItems= 'center';
+divDescription.style.gap= '12px';
+divDescription.style.flexGrow = '1'
+divDescription.style.flexShrink = '1'
+
+// pProductName.innerHTML = "Tacos";
+pProductName.style.fontSize = '16px';
+pProductName.style.padding = '0';
+pProductName.style.margin = '0';
+pPriceProduct.style.padding = '0';
+pPriceProduct.style.margin = '0';
+// pPriceProduct.innerHTML = "15€"
+pPriceProduct.style.fontSize = '12px';
+
+
+
+buttonCard.style.width = '40px'
+buttonCard.style.fontSize = '12px'
+buttonCard.style.height = '20px'
+buttonCard.innerText = 'info'
+
+containerBodyFood.appendChild(divContainer);
+
+buttonCard.addEventListener('click',() => {
+const nuevaOrden = {img:item.img,name: item.product_Name,
+price: item.price,
+}
+
+carrito.push(nuevaOrden);
+})
+
+for (item of carrito){
+div.appendChild(display)
+const display = document.createElement('div')
+display.width = '150px';
+display.height = '40px';
+const imgCarrito = document.createElement('img');
+imgCarrito.setAttribute('src',item.img);
+const titutloOrden = document.createElement('p');
+titutloOrden.innerHTML = item.product_Name;
+display.appendChild(imgCarrito);
+display,appendChild(titutloOrden);
+}
+}
+
+console.log(carrito);
+
+/* lograr hacer un nuevo array de objetos, cada vez que se de click, en vez log, que eso se añada en un array y eso se debe mostrar en el div que oculta. */
